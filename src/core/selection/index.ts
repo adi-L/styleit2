af6e1db988c22b847997100cd47b174f1c2e595b
style-it <styleit.api@gmail.com>
2021-12-03 17:29:45 +0200

    from if to while

--- src/core/selection/index.ts
+++ src/core/selection/index.ts
@@ -6,16 +6,7 @@ import { splitHtml } from '../scripts/splitHtml';
 import Command from '../interfaces/command';
 import unWrap from '../scripts/unwrap';
 import mergeSiblings from '../scripts/mergeSiblings';
-import insertAfter from '../scripts/insertAfter';
-// function isSelectionInTag(selector: String) {
-//     // Get the current node
-//     let currentNode = window.getSelection().focusNode;
-//     // While the node is not the editor division
-//     if (currentNode.nodeType === 1 && (currentNode as any).closest(selector)) {
-//         return true;
-//     }
-//     return false;
-// }
+
 const wrapNodes = (target: Element, command: Command): Element[] | undefined => {
     const { nodeName, splitDom = [] } = command;
 
@@ -33,14 +24,9 @@ const wrapNodes = (target: Element, command: Command): Element[] | undefined => 
         return [];
     }
     const wrapperEl: Element = document.createElement(nodeName as any);
-    let isToggle: Boolean;
-    const onBeforeWrappNode = (node: Element, index: Number) => {
-        if (index === 0) {
-            isToggle = !!getClosestSameNode(node, splitDom);
-        }
-    }
     const onWrapNode = (node: Element) => {
         let parent = getClosestSameNode(node, splitDom);
+
         while (parent) {
             const toElement = parent;
             parent = getClosestSameNode(node, splitDom);
@@ -57,29 +43,27 @@ const wrapNodes = (target: Element, command: Command): Element[] | undefined => 
                 }
             }
         }
-        if (isToggle) {
-            node.setAttribute("data-remove", "true");
-        }
     }
-    const nodes: Node[] = createWrapperFunction(wrapperEl, range, onWrapNode, onBeforeWrappNode);
-    Array.from(target.querySelectorAll("[data-remove]")).forEach(el => unWrap(el));
-    if (startFlag && startFlag.nextSibling && startFlag.nextSibling.nodeType === 3 && typeof startFlag.nextSibling.textContent === "string" && !startFlag.nextSibling.textContent.trim()) {
+
+    const nodes: Node[] = createWrapperFunction(wrapperEl, range, onWrapNode);
+  
+    while (startFlag && startFlag.nextSibling && startFlag.nextSibling.nodeType === 3 && typeof startFlag.nextSibling.textContent === "string" && !startFlag.nextSibling.textContent.trim()) {
         startFlag.nextSibling.remove();
     }
     if (startFlag.nextSibling && startFlag.nextSibling.nodeType === 1) {
         (startFlag as any).nextSibling.prepend(startFlag);
     }
-    if (endFlag && endFlag.previousSibling && endFlag.previousSibling.nodeType === 3 && typeof endFlag.previousSibling.textContent === "string" && !endFlag.previousSibling.textContent.trim()) {
+    while (endFlag && endFlag.previousSibling && endFlag.previousSibling.nodeType === 3 && typeof endFlag.previousSibling.textContent === "string" && !endFlag.previousSibling.textContent.trim()) {
         endFlag.previousSibling.remove();
     }
     if (endFlag.previousSibling && endFlag.previousSibling.nodeType === 1) {
         (endFlag as any).previousSibling.appendChild(endFlag);
     }
-    nodes.forEach((node: Node) => {
-        if (node && node.parentElement) {
-            mergeSiblings(node.parentElement);
-        }
-    });
+    if (range.commonAncestorContainer) {
+        Array.from(range.commonAncestorContainer.childNodes).forEach((node: Node) => {
+            mergeSiblings(node);
+        });
+    }
     createSelectionBetweenTwoNodes(startFlag, endFlag);
     startFlag.parentNode && startFlag.parentNode.removeChild(startFlag);
     endFlag.parentNode && endFlag.parentNode.removeChild(endFlag);
